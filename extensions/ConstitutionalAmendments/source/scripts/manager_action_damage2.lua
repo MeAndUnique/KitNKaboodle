d9179ec2791afc4cf1951414e992b16e75c2fd9a
Shane Parker <shane.tim.parker@gmail.com>
2021-01-17 17:54:11 +0000

    Fixed bug with life steal damage not being calculated from initial roll on critical hits.

--- extensions/ConstitutionalAmendments/source/scripts/manager_action_damage2.lua
+++ extensions/ConstitutionalAmendments/source/scripts/manager_action_damage2.lua
@@ -37,13 +37,15 @@ end
 
 function messageDamage(rSource, rTarget, bSecret, sDamageType, sDamageDesc, sTotal, sExtraResult)
 	local nStolen = 0;
-	local bStealTemp = false;
+	local nTempStolen = 0;
 	if decodeResult and decodeResult.sType == "damage" then
 		local sTargetType, nodeTarget = ActorManager.getTypeAndNode(rTarget);
+		local nMax = 0;
 		for sTypes,nDamage in pairs(decodeResult.aDamageTypes) do
 			local aTemp = StringManager.split(sTypes, ",", true);
 			local bMax = false;
 			local nSteal = 0;
+			local nTempSteal = 0;
 			for _,type in ipairs(aTemp) do
 				bMax = bMax or type == "max";
 				if type == "steal" then
@@ -52,54 +54,57 @@ function messageDamage(rSource, rTarget, bSecret, sDamageType, sDamageDesc, sTot
 					nSteal = 0.5;
 				end
 				if type == "stealtemp" then
-					nSteal = 1;
-					bStealTemp = true;
+					nTempSteal = 1;
 				elseif type == "hstealtemp" then
-					nSteal = 0.5;
-					bStealTemp = true;
+					nTempSteal = 0.5;
 				end
 			end
 
-			if bMax or (nSteal > 0) then
+			if bMax or (nSteal > 0) or (nTempSteal > 0) then
 				local rDamageOutput = {aDamageTypes={[sTypes]=nDamage}};
 				local nDamageAdjust = ActionDamage.getDamageAdjust(rSource, rTarget, nDamage, rDamageOutput);
 				nDamageAdjust = nDamageAdjust + nDamage;
-				nStolen = math.floor(nDamageAdjust * nSteal);
+				nStolen = nStolen + math.floor(nDamageAdjust * nSteal);
+				nTempStolen = nTempStolen + math.floor(nDamageAdjust * nTempSteal)
 
 				if bMax and (nDamageAdjust > 0) then
-					sExtraResult = sExtraResult .. " [MAX REDUCED]";
-					if sTargetType == "pc" then
-						local nAdjust = DB.getValue(nodeTarget, "hp.adjust", 0) - nDamageAdjust;
-						DB.setValue(nodeTarget, "hp.adjust", "number", nAdjust);
-						HpManager.recalculateTotal(nodeTarget);
-						
-						local nTotal = DB.getValue(nodeTarget, "hp.total", 0);
-						if nTotal <= 0 then
-							if not string.match(sExtraResult, "%[INSTANT DEATH%]") then
-								sExtraResult = sExtraResult .. " [INSTANT DEATH]";
-							end
-							nAdjust = nAdjust - nTotal;
-							DB.setValue(nodeTarget, "hp.total", "number", 0);
-							DB.setValue(nodeTarget, "hp.adjust", "number", nAdjust);
-							DB.setValue(nodeTarget, "hp.deathsavefail", "number", 3);
-						end
-
-						local nWounds = DB.getValue(nodeTarget, "hp.wounds", 0);
-						DB.setValue(nodeTarget, "hp.wounds", "number", math.max(0, nWounds - nDamageAdjust));
-					else
-						local nTotal = DB.getValue(nodeTarget, "hptotal", 0) - nDamageAdjust;
-						if nTotal < 0 then
-							if not string.match(sExtraResult, "%[INSTANT DEATH%]") then
-								sExtraResult = sExtraResult .. " [INSTANT DEATH]";
-							end
-							nTotal = 0;
-						end
-						DB.setValue(nodeTarget, "hptotal", "number", nTotal);
-
-						local nWounds = DB.getValue(nodeTarget, "wounds", 0);
-						DB.setValue(nodeTarget, "wounds", "number", math.max(0, nWounds - nDamageAdjust));
-					end
+					nMax = nMax + nDamageAdjust;
 				end
+			end
+		end
+
+		if nMax > 0 then
+			sExtraResult = sExtraResult .. " [MAX REDUCED]";
+			if sTargetType == "pc" then
+				local nAdjust = DB.getValue(nodeTarget, "hp.adjust", 0) - nMax;
+				DB.setValue(nodeTarget, "hp.adjust", "number", nAdjust);
+				HpManager.recalculateTotal(nodeTarget);
+				
+				local nTotal = DB.getValue(nodeTarget, "hp.total", 0);
+				if nTotal <= 0 then
+					if not string.match(sExtraResult, "%[INSTANT DEATH%]") then
+						sExtraResult = sExtraResult .. " [INSTANT DEATH]";
+					end
+					nAdjust = nAdjust - nTotal;
+					DB.setValue(nodeTarget, "hp.total", "number", 0);
+					DB.setValue(nodeTarget, "hp.adjust", "number", nAdjust);
+					DB.setValue(nodeTarget, "hp.deathsavefail", "number", 3);
+				end
+
+				local nWounds = DB.getValue(nodeTarget, "hp.wounds", 0);
+				DB.setValue(nodeTarget, "hp.wounds", "number", math.max(0, nWounds - nMax));
+			else
+				local nTotal = DB.getValue(nodeTarget, "hptotal", 0) - nMax;
+				if nTotal < 0 then
+					if not string.match(sExtraResult, "%[INSTANT DEATH%]") then
+						sExtraResult = sExtraResult .. " [INSTANT DEATH]";
+					end
+					nTotal = 0;
+				end
+				DB.setValue(nodeTarget, "hptotal", "number", nTotal);
+
+				local nWounds = DB.getValue(nodeTarget, "wounds", 0);
+				DB.setValue(nodeTarget, "wounds", "number", math.max(0, nWounds - nMax));
 			end
 		end
 	elseif string.match(sDamageDesc, "%[STOLEN%]") then
@@ -111,9 +116,10 @@ function messageDamage(rSource, rTarget, bSecret, sDamageType, sDamageDesc, sTot
 
 	if nStolen > 0 then
 		local sDamage = "[HEAL][STOLEN]";
-		if bStealTemp then
-			sDamage = sDamage .. "[TEMP]";
-		end
 		ActionDamage.applyDamage(rSource, rSource, bSecret, sDamage, nStolen);
+	end
+	if nTempStolen > 0 then
+		local sDamage = "[HEAL][STOLEN][TEMP]";
+		ActionDamage.applyDamage(rSource, rSource, bSecret, sDamage, nTempStolen);
 	end
 end
