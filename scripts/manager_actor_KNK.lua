448af8dca4b5ff3880b07c100662c1b3049954cd
MeAndUnique <shane.tim.parker@gmail.com>
2023-02-04 08:34:43 -0500

    Fixed blank unowned item ability text

--- scripts/manager_actor_KNK.lua
+++ scripts/manager_actor_KNK.lua
@@ -9,12 +9,14 @@ local getActorRecordTypeFromPathOriginal;
 function onInit()
 	getActorRecordTypeFromPathOriginal = ActorManager.getActorRecordTypeFromPath;
 	ActorManager.getActorRecordTypeFromPath = getActorRecordTypeFromPath;
+
+	ActorManager.registerActorRecordType("item");
 end
 
 -- Internal use only
 function getActorRecordTypeFromPath(sActorNodePath)
 	if sActorNodePath:match("%.inventorylist%.") then
-		return nil;
+		return "item";
 	else
 		return getActorRecordTypeFromPathOriginal(sActorNodePath);
 	end
