9d483ec4d628afd07c1ea71ee0c8c0267978ab05
MeAndUnique <shane.tim.parker@gmail.com>
2023-02-26 12:10:35 -0500

    Added handling of score changes for all action displays

--- campaign/scripts/power_item_actionlist.lua
+++ campaign/scripts/power_item_actionlist.lua
@@ -14,12 +14,40 @@ function onInit()
 	local nodePower = window.getDatabaseNode();
 	DB.addHandler(nodePower.getPath("actions"), "onChildDeleted", onActionDeleted);
 
+	local nodeChar = PowerManagerCore.getPowerActorNode(nodePower);
+	DB.addHandler(DB.getPath(nodeChar, "level"), "onUpdate", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "abilities.*.score"), "onUpdate", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "powergroup.*.stat"), "onUpdate", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "powergroup.*.atkstat"), "onUpdate", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "powergroup.*.atkprof"), "onUpdate", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "powergroup.*.atkmod"), "onUpdate", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "powergroup.*.savestat"), "onUpdate", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "powergroup.*.saveprof"), "onUpdate", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "powergroup.*.savemod"), "onUpdate", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "powergroup"), "onChildAdded", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "powergroup"), "onChildDeleted", onAbilityChanged);
+	DB.addHandler(DB.getPath(nodeChar, "powergroup.*.name"), "onUpdate", onAbilityChanged);
+
 	PowerManagerKNK.fillActionOrderGap(window.getDatabaseNode());
 end
 
 function onClose()
 	local nodePower = window.getDatabaseNode();
 	DB.removeHandler(nodePower.getPath("actions"), "onChildDeleted", onActionDeleted);
+	
+	local nodeChar = PowerManagerCore.getPowerActorNode(nodePower);
+	DB.removeHandler(DB.getPath(nodeChar, "level"), "onUpdate", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "abilities.*.score"), "onUpdate", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "powergroup.*.stat"), "onUpdate", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "powergroup.*.atkstat"), "onUpdate", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "powergroup.*.atkprof"), "onUpdate", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "powergroup.*.atkmod"), "onUpdate", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "powergroup.*.savestat"), "onUpdate", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "powergroup.*.saveprof"), "onUpdate", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "powergroup.*.savemod"), "onUpdate", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "powergroup"), "onChildAdded", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "powergroup"), "onChildDeleted", onAbilityChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "powergroup.*.name"), "onUpdate", onAbilityChanged);
 
 	if super and super.onClose then
 		super.onClose();
@@ -37,4 +65,8 @@ function update(bNewReadOnly, bNewHideCast)
 	for _,win in ipairs(getWindows()) do
 		win.update(bReadOnly, bHideCast);
 	end
+end
+
+function onAbilityChanged()
+	WindowManager.callInnerFunction(self, "onDataChanged");
 end
